b71f63f735767aec54ea108b0588a37d8dfed145
Artem Satanovskiy <artsatanov@gmail.com>
2023-08-29 02:14:17 +0300

    prep

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+
+const refs = {
+dropDownMenu: document.querySelector ('.breed-select'),
+};
+
+function catsAPIRequester() {
+   const params = new URLSearchParams({
+      x-api-key: live_fRWteppQp5QuzzEUJhf1msveKO9JBnOqUGQgzkqPKAfAKyfAENHU7maeb3sfPrvh,
+   }); 
+};
+
