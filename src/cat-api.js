dc1a6e122f7e83f1c35d836315eecd7960bb1883
Artem Satanovskiy <artsatanov@gmail.com>
2023-08-29 03:20:22 +0300

    1st

---
+++ src/cat-api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+function fetchBreeds() {
+axios({
+  method: 'get',
+  url: 'https://api.thecatapi.com/v1/breeds',
+})
+   .then((resp) => {
+      // console.log(resp);
+      console.log(resp.data);
+   }
+   )
+   .catch(() => {
+      console.log('Something went wrong!!!');
+   }
+   );
+};
+
+
+export {fetchBreeds};
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_fRWteppQp5QuzzEUJhf1msveKO9JBnOqUGQgzkqPKAfAKyfAENHU7maeb3sfPrvh";
+
+import { fetchBreeds } from "./cat-api.js";
 
 
 const refs = {
 dropDownMenu: document.querySelector ('.breed-select'),
 };
 
-function catsAPIRequester() {
-   const params = new URLSearchParams({
-      x-api-key: live_fRWteppQp5QuzzEUJhf1msveKO9JBnOqUGQgzkqPKAfAKyfAENHU7maeb3sfPrvh,
-   }); 
-};
+fetchBreeds();
 
+// console.log(fetchBreeds())
