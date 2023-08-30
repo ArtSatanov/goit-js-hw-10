52aeb20b030d87b070c336ca98b277f6b1401807
ArtSatanov <artsatanov@gmail.com>
2023-08-31 02:24:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,73 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_fRWteppQp5QuzzEUJhf1msveKO9JBnOqUGQgzkqPKAfAKyfAENHU7maeb3sfPrvh";
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_fRWteppQp5QuzzEUJhf1msveKO9JBnOqUGQgzkqPKAfAKyfAENHU7maeb3sfPrvh';
 
-import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
-
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const refs = {
-   dropDownMenu: document.querySelector('.breed-select'),
-   container: document.querySelector('.cat-info'),
-   loader: document.querySelector('.loader'),
-   error: document.querySelector('.error')
+  dropDownMenu: document.querySelector('.breed-select'),
+  container: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
 };
 
 function catsSelectorMarkup(arr) {
-   const markup = arr.map(({ id,name }) => `<option value="${id}">${name}</option>` ).join('');
-   refs.dropDownMenu.insertAdjacentHTML('beforeend', markup);
+  const markup = arr
+    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+    .join('');
+  refs.dropDownMenu.insertAdjacentHTML('beforeend', markup);
 }
 
 function cantInfoMarkupPic(arr) {
-   const markup = arr.map(({ url }) =>
-      `<img src="${url}" alt="Cat" width="300" style="margin-top:20px">
-   ` ).join('');
-   refs.container.insertAdjacentHTML('afterbegin', markup)
+  const markup = arr
+    .map(
+      ({ url }) =>
+        `<div style="margin-top:20px ">
+      <img src="${url}" alt="Cat" width="300" style="margin-top:20px display: block">
+      </div>
+   `
+    )
+    .join('');
+  refs.container.insertAdjacentHTML('afterbegin', markup);
 }
 
 function cantInfoMarkup(arr) {
-   const markup = arr.map(({ name,description,temperament }) =>
-      `<div>
-      <h2>${name}</h2>
+  const markup = arr
+    .map(
+      ({ name, description, temperament }) =>
+        `<div style="margin-top:20px ">
+         <h2>${name}</h2>
          <p>${description}</p>
          <p><span style="font-weigth:500">Temperament:</span> ${temperament}</p>
       </div>
-   ` ).join('');
-   refs.container.insertAdjacentHTML('beforeend', markup)
+   `
+    )
+    .join('');
+  refs.container.insertAdjacentHTML('beforeend', markup);
 }
 
-fetchBreeds() 
-   .then((results) => {
-      catsSelectorMarkup(results.data);
-   }
-)
+fetchBreeds().then(results => {
+  catsSelectorMarkup(results.data);
+});
 
+refs.dropDownMenu.addEventListener('change', event => {
+  refs.container.innerHTML = '';
+  refs.loader.hidden = false;
 
-refs.dropDownMenu.addEventListener('change', (event) => {
-   refs.container.innerHTML = '';
-   refs.loader.hidden = false;
-   
-   let targetId = event.target.value
-   console.log(targetId);
+  let targetId = event.target.value;
+  console.log(targetId);
 
-   fetchCatByBreed(targetId)
-      .then((results) => {
-         refs.loader.hidden = true;
-         console.log(results.data)
-         console.log(results.data[0].breeds)
-         cantInfoMarkupPic(results.data);
-         cantInfoMarkup(results.data[0].breeds);
+  fetchCatByBreed(targetId)
+    .then(results => {
+       refs.loader.hidden = true;
+       refs.container.setAttribute("style", "display:flex gap:40px");
 
-      })
-      .catch(() => { 
-         refs.error.hidden = false;
-      })
-})
-
-
-
-
+      console.log(results.data);
+      console.log(results.data[0].breeds);
+      cantInfoMarkupPic(results.data);
+      cantInfoMarkup(results.data[0].breeds);
+    })
+    .catch(() => {
+      refs.error.hidden = false;
+    });
+});
