9667ed90a01427cd1263a31c648b9650a5757bd1
ArtSatanov <artsatanov@gmail.com>
2023-08-31 01:41:16 +0300

    task

--- src/index.js
+++ src/index.js
@@ -16,8 +16,21 @@ function catsSelectorMarkup(arr) {
    refs.dropDownMenu.insertAdjacentHTML('beforeend', markup);
 }
 
-function canInfoMarkup(arr) {
-   const markup = arr.map(({ url }) => `<img src="${url}" alt="Cat" width="300" style="margin-top:20px">` ).join('');
+function cantInfoMarkupPic(arr) {
+   const markup = arr.map(({ url }) =>
+      `<img src="${url}" alt="Cat" width="300" style="margin-top:20px">
+   ` ).join('');
+   refs.container.insertAdjacentHTML('afterbegin', markup)
+}
+
+function cantInfoMarkup(arr) {
+   const markup = arr.map(({ name,description,temperament }) =>
+      `<div>
+      <h2>${name}</h2>
+         <p>${description}</p>
+         <p><span style="font-weigth:500">Temperament:</span> ${temperament}</p>
+      </div>
+   ` ).join('');
    refs.container.insertAdjacentHTML('beforeend', markup)
 }
 
@@ -37,13 +50,18 @@ refs.dropDownMenu.addEventListener('change', (event) => {
 
    fetchCatByBreed(targetId)
       .then((results) => {
-         refs.loader.hidden = true; 
-         canInfoMarkup(results.data);
+         refs.loader.hidden = true;
+         console.log(results.data)
+         console.log(results.data[0].breeds)
+         cantInfoMarkupPic(results.data);
+         cantInfoMarkup(results.data[0].breeds);
 
       })
       .catch(() => { 
-         refs.error.hidden = true;
+         refs.error.hidden = false;
       })
 })
+
+
 
 
