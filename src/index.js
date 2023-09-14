14c41874226156f8c3b04cf3d7b2a7135a9c39aa
ArtSatanov <artsatanov@gmail.com>
2023-09-14 21:51:24 +0300

    1

--- src/index.js
+++ src/index.js
@@ -8,70 +8,77 @@ const refs = {
   container: document.querySelector('.cat-info'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
+  input: document.querySelector('input[checkbox]'),
 };
+  
+console.log(refs);
 
-function catsSelectorMarkup(arr) {
-  const markup = arr
-    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-    .join('');
-  refs.dropDownMenu.insertAdjacentHTML('beforeend', markup);
-}
+// function catsSelectorMarkup(arr) {
+//   const markup = arr
+//     .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+//     .join('');
+//   refs.dropDownMenu.insertAdjacentHTML('beforeend', markup);
+// }
 
-function cantInfoMarkupPic(arr) {
-  const markup = arr
-    .map(
-      ({ url }) =>
-        `<div style="margin-top:20px ">
-      <img src="${url}" alt="Cat" width="300" style="margin-top:20px display: block">
-      </div>
-   `
-    )
-    .join('');
-  refs.container.insertAdjacentHTML('afterbegin', markup);
-}
+// function cantInfoMarkupPic(arr) {
+//   const markup = arr
+//     .map(
+//       ({ url }) =>
+//         `<div style="margin-top:20px ">
+//       <img src="${url}" alt="Cat" width="300" style="margin-top:20px display: block">
+//       </div>
+//    `
+//     )
+//     .join('');
+//   refs.container.insertAdjacentHTML('afterbegin', markup);
+// }
 
-function cantInfoMarkup(arr) {
-  const markup = arr
-    .map(
-      ({ name, description, temperament }) =>
-        `<div style="margin-top:20px; width:600px;">
-         <h2 style="margin-top:0">${name}</h2>
-         <p>${description}</p>
-         <p><span style="font-weight: 800;">Temperament:</span> ${temperament}</p>
-      </div>
-   `
-    )
-    .join('');
-  refs.container.insertAdjacentHTML('beforeend', markup);
-}
+// function cantInfoMarkup(arr) {
+//   const markup = arr
+//     .map(
+//       ({ name, description, temperament }) =>
+//         `<div style="margin-top:20px; width:600px;">
+//          <h2 style="margin-top:0">${name}</h2>
+//          <p>${description}</p>
+//          <p><span style="font-weight: 800;">Temperament:</span> ${temperament}</p>
+//       </div>
+//    `
+//     )
+//     .join('');
+//   refs.container.insertAdjacentHTML('beforeend', markup);
+// }
 
-fetchBreeds()
-  .then(results => {
-  catsSelectorMarkup(results.data);
-  new SlimSelect({
-  select: refs.dropDownMenu,
-  })
-});
+// fetchBreeds()
+//   .then(results => {
+//   catsSelectorMarkup(results.data);
+//   new SlimSelect({
+//   select: refs.dropDownMenu,
+//   })
+// });
 
-refs.dropDownMenu.addEventListener('change', event => {
-  refs.container.innerHTML = '';
-  refs.loader.hidden = false;
+// refs.dropDownMenu.addEventListener('change', event => {
+//   refs.container.innerHTML = '';
+//   refs.loader.hidden = false;
 
-  let targetId = event.target.value;
-  console.log(targetId);
-  fetchCatByBreed(targetId)
-    .then(results => {
-      if (results.data.length > 0) {
-       refs.loader.hidden = true;
-       refs.container.setAttribute("style", "display:flex; gap:20px; flex-direction:row;");
-      console.log(results.data);
-      console.log(results.data[0].breeds);
-      cantInfoMarkupPic(results.data);
-      cantInfoMarkup(results.data[0].breeds);
-      } else {
-        refs.loader.hidden = true;
-        Notify.failure('Oops! Something went wrong! Try reloading the page!');
-      }
+//   let targetId = event.target.value;
+//   console.log(targetId);
+//   fetchCatByBreed(targetId)
+//     .then(results => {
+//       if (results.data.length > 0) {
+//        refs.loader.hidden = true;
+//        refs.container.setAttribute("style", "display:flex; gap:20px; flex-direction:row;");
+//       console.log(results.data);
+//       console.log(results.data[0].breeds);
+//       cantInfoMarkupPic(results.data);
+//       cantInfoMarkup(results.data[0].breeds);
+//       } else {
+//         refs.loader.hidden = true;
+//         Notify.failure('Oops! Something went wrong! Try reloading the page!');
+//       }
       
-    })
-});
+//     })
+// });
+
+function themeChanger() {
+  
+}
